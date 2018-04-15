9b5c9e4f84e4ce5661e9dfb532a2dd10751cc505
madron <massimiliano.ravelli@gmail.com>
2018-04-16 00:06:01 +0200

    added nuxt-pouch module

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,9 @@ module.exports = {
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
+  modules: [
+    'nuxt-pouch'
+  ],
   plugins: ['~/plugins/vuetify.js'],
   css: [
     '~/assets/style/app.styl'
